87f8a7146ced49a11a9a0cf384050697ccd21323
mustofa-binary <mustofa@regentmarkets.com>
2019-09-12 11:10:28 +0800

    add product dtail

---
+++ src/pages/product-detail/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import PropTypes from 'prop-types'
+// @material-ui/core components
+import withStyles from '@material-ui/co/styles/withStyles'
+import GridContainer from '../../components/shared/Grid/GridContainer.jsx'
+
+import productsDetailStyle from '../../components/jss/maritim/views/productDetailPage.jsx'
+
+import Layout from '../../components/layout/layout.js'
+import SEO from '../../components/layout/seo.js'
+
+class ProductDetail extends React.Component {
+    state = {}
+    render() {
+        const { classes } = this.props
+        return (
+            <Layout>
+                <SEO title="product detail" />
+                <div className={classes.header}></div>
+            </Layout>
+        )
+    }
+}
+
+export default withStyles(productsDetailStyle)(ProductDetail)
