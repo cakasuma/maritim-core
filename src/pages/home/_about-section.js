fe23bab8bc7ef790d243e5856307e8e390eacb8c
cakasuma <amammustofa@gmail.com>
2019-10-14 22:37:15 +0800

    update content

--- src/pages/home/_about-section.js
+++ src/pages/home/_about-section.js
@@ -2,14 +2,25 @@ import React from 'react'
 // @material-ui/core components
 import { makeStyles } from '@material-ui/core/styles'
 
+import { navigate } from 'gatsby'
+
 // @material-ui/icons
-import ThumbUp from '@material-ui/icons/ThumbUp'
+import BarChart from '@material-ui/icons/BarChart'
 import VerifiedUser from '@material-ui/icons/VerifiedUser'
-import ContactPhone from '@material-ui/icons/ContactPhone'
+import Class from '@material-ui/icons/Class'
+import Chat from '@material-ui/icons/Chat'
 // core components
 import GridContainer from '../../components/shared/Grid/GridContainer.jsx'
 import GridItem from '../../components/shared/Grid/GridItem.jsx'
 import InfoArea from '../../components/shared/InfoArea/InfoArea.jsx'
+
+import Energy from '@material-ui/icons/Autorenew'
+import Biomedic from '@material-ui/icons/NaturePeople'
+import IT from '@material-ui/icons/DevicesOther'
+import Food from '@material-ui/icons/Fastfood'
+import Other from '@material-ui/icons/CardGiftcard'
+
+import NavPillsStatic from '../../components/shared/NavPillsStatic/NavPillsStatic.jsx'
 
 import styles from '../../components/jss/maritim/views/landingPageSections/productStyle.jsx'
 
@@ -19,44 +30,91 @@ export default function AboutSection() {
     const classes = useStyles()
     return (
         <div className={classes.section}>
-            <GridContainer justify="center">
-                <GridItem xs={12} sm={12} md={8}>
-                    <h2 className={classes.title}>
-                        Let{"'"}s talk about innovation
-                    </h2>
-                    <h5 className={classes.description}>
-                        The innovation hosted in this platform comes from
-                        verified students and source, which are worth to become
-                        your next investment
-                    </h5>
+            <GridContainer className={classes.container}>
+                <GridItem xs={12} sm={12} md={12}>
+                    <h2 className={classes.title2}>Choose the categories</h2>
+                    <NavPillsStatic
+                        alignCenter
+                        tabs={[
+                            {
+                                tabButton: 'Energy',
+                                tabIcon: Energy,
+                                color: 'success',
+                                onClick: () => {
+                                    navigate('/products?cat=energy')
+                                },
+                            },
+                            {
+                                tabButton: 'IT',
+                                tabIcon: IT,
+                                color: 'warning',
+                                onClick: () => {
+                                    navigate('/products?cat=it')
+                                },
+                            },
+                            {
+                                tabButton: 'Biomedic',
+                                tabIcon: Biomedic,
+                                color: 'danger',
+                                onClick: () => {
+                                    navigate('/products?cat=biomedic')
+                                },
+                            },
+                            {
+                                tabButton: 'Food',
+                                tabIcon: Food,
+                                color: 'rose',
+                                onClick: () => {
+                                    navigate('/products?cat=food')
+                                },
+                            },
+                            {
+                                tabButton: 'Others',
+                                tabIcon: Other,
+                                color: 'info',
+                                onClick: () => {
+                                    navigate('/products?cat=others')
+                                },
+                            },
+                        ]}
+                    />
                 </GridItem>
             </GridContainer>
-            <div>
+            <div className={classes.separator}>
                 <GridContainer>
-                    <GridItem xs={12} sm={12} md={4}>
+                    <GridItem xs={12} sm={12} md={3}>
                         <InfoArea
-                            title="Verified students"
-                            description="We verify students background before they can submit their innovation. despite of it, it will be easy for innovator to create new innovation with us"
+                            title="Free chat"
+                            description="Help innovators and stakeholders to communicate for the further support of each other"
+                            icon={Chat}
+                            iconColor="rose"
+                            vertical
+                        />
+                    </GridItem>
+                    <GridItem xs={12} sm={12} md={3}>
+                        <InfoArea
+                            title="Verified Users"
+                            description="The innovators and the stakeholders registered and verified from the company"
                             icon={VerifiedUser}
-                            iconColor="info"
+                            iconColor="rose"
                             vertical
                         />
                     </GridItem>
-                    <GridItem xs={12} sm={12} md={4}>
+                    <GridItem xs={12} sm={12} md={3}>
                         <InfoArea
-                            title="Trusted source"
-                            description="We take a look at geuninity of the innovation seriously. as it will benefits both of the parties in continuation of the idea."
-                            icon={ThumbUp}
-                            iconColor="success"
+                            title="Various Fields"
+                            description="Accommodate various fields of innovation for Indonesia's innovators and stakeholders"
+                            icon={BarChart}
+                            iconColor="rose"
                             vertical
                         />
                     </GridItem>
-                    <GridItem xs={12} sm={12} md={4}>
+                    <GridItem xs={12} sm={12} md={3}>
                         <InfoArea
-                            title="Easily accessible"
-                            description="the innovation will be available in our platform when innovator upload it to our system."
-                            icon={ContactPhone}
-                            iconColor="danger"
+                            title="Patent Status"
+                            description="Help the innovators to get a patent for the innovation as an Intellectual Property of Indonesia"
+                            icon={Class}
+                            iconColor="rose"
                             vertical
                         />
                     </GridItem>
