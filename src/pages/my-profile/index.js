a28491ed8bf556c850db9511f8ba392085499b6b
cakasuma <amammustofa@gmail.com>
2019-10-20 04:24:46 +0800

    update profile

--- src/pages/my-profile/index.js
+++ src/pages/my-profile/index.js
@@ -1,219 +1,110 @@
-import React from "react";
+import React from 'react'
 // nodejs library that concatenates classes
-import classNames from "classnames";
-import {navigate} from 'gatsby'
+import classNames from 'classnames'
+import { navigate } from 'gatsby'
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles'
 // @material-ui/icons
-import Camera from "@material-ui/icons/Camera";
-import Palette from "@material-ui/icons/Palette";
-import Favorite from "@material-ui/icons/Favorite";
+import Camera from '@material-ui/icons/Camera'
+import Palette from '@material-ui/icons/Palette'
+import Favorite from '@material-ui/icons/Favorite'
 import { FirebaseContext } from 'gatsby-plugin-firebase'
 // core components
 import Layout from '../../components/layout/layout.js'
-import Button from "../../components/shared/CustomButtons/Button.jsx";
-import GridContainer from "../../components/shared/Grid/GridContainer.jsx";
-import GridItem from "../../components/shared/Grid/GridItem.jsx";
-import NavPills from "../../components/shared/NavPills/NavPills.jsx";
-import Parallax from "../../components/shared/Parallax/Parallax.jsx";
+import Button from '../../components/shared/CustomButtons/Button.jsx'
+import GridContainer from '../../components/shared/Grid/GridContainer.jsx'
+import GridItem from '../../components/shared/Grid/GridItem.jsx'
+import NavPills from '../../components/shared/NavPills/NavPills.jsx'
+import Parallax from '../../components/shared/Parallax/Parallax.jsx'
 
-import profile from "../../images/faces/christian.jpg";
+import profile from '../../images/faces/christian.jpg'
 
-import studio1 from "../../images/examples/studio-1.jpg";
-import studio2 from "../../images/examples/studio-2.jpg";
-import studio3 from "../../images/examples/studio-3.jpg";
-import studio4 from "../../images/examples/studio-4.jpg";
-import studio5 from "../../images/examples/studio-5.jpg";
-import work1 from "../../images/examples/olu-eletu.jpg";
-import work2 from "../../images/examples/clem-onojeghuo.jpg";
-import work3 from "../../images/examples/cynthia-del-rio.jpg";
-import work4 from "../../images/examples/mariya-georgieva.jpg";
-import work5 from "../../images/examples/clem-onojegaw.jpg";
+import studio1 from '../../images/examples/studio-1.jpg'
+import studio2 from '../../images/examples/studio-2.jpg'
+import studio3 from '../../images/examples/studio-3.jpg'
+import studio4 from '../../images/examples/studio-4.jpg'
+import studio5 from '../../images/examples/studio-5.jpg'
+import work1 from '../../images/examples/olu-eletu.jpg'
+import work2 from '../../images/examples/clem-onojeghuo.jpg'
+import work3 from '../../images/examples/cynthia-del-rio.jpg'
+import work4 from '../../images/examples/mariya-georgieva.jpg'
+import work5 from '../../images/examples/clem-onojegaw.jpg'
 import styles from '../../components/jss/maritim/views/profilePage.jsx'
 
-const useStyles = makeStyles(styles);
+const useStyles = makeStyles(styles)
 
 export default function ProfilePage(props) {
-  const firebase = React.useContext(FirebaseContext)
-  const [currentUser, setCurrentUser] = React.useState()
-  const classes = useStyles();
-  const { ...rest } = props;
-  const imageClasses = classNames(
-    classes.imgRaised,
-    classes.imgRoundedCircle,
-    classes.imgFluid
-  );
-  const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
-  
-  React.useEffect(() => {
-    if (!firebase) {
-        return
-    }
+    const firebase = React.useContext(FirebaseContext)
+    const [user, setUser] = React.useState()
+    const classes = useStyles()
+    const { ...rest } = props
+    const imageClasses = classNames(
+        classes.imgRaised,
+        classes.imgRoundedCircle,
+        classes.imgFluid,
+    )
+    const navImageClasses = classNames(classes.imgRounded, classes.imgGallery)
 
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (!user) {
-            navigate('/login')
+    React.useEffect(() => {
+        if (!firebase) {
             return
         }
-        setCurrentUser(user)
-    })
-}, [firebase])
 
-  return (
-    <Layout>
-      <Parallax small filter image={require("../../images/profile-bg.jpg")} />
-      <div className={classNames(classes.main, classes.mainRaised)}>
-        <div>
-          <div className={classes.container}>
-            <GridContainer justify="center">
-              <GridItem xs={12} sm={12} md={6}>
-                <div className={classes.profile}>
-                  <div>
-                    <img src={profile} alt="..." className={imageClasses} />
-                  </div>
-                  <div className={classes.name}>
-                    <h3 className={classes.title}>Christian Louboutin</h3>
-                    <h6>DESIGNER</h6>
-                    <Button justIcon link className={classes.margin5}>
-                      <i className={"fab fa-twitter"} />
-                    </Button>
-                    <Button justIcon link className={classes.margin5}>
-                      <i className={"fab fa-instagram"} />
-                    </Button>
-                    <Button justIcon link className={classes.margin5}>
-                      <i className={"fab fa-facebook"} />
-                    </Button>
-                  </div>
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                console.log(user)
+
+                const db = firebase.firestore()
+                const query = db.collection('users').doc(user.uid)
+
+                query.get().then(hasil => {
+                    setUser(hasil.data())
+                })
+            } else {
+                navigate('/login')
+                return
+            }
+        })
+    }, [firebase])
+
+    return (
+        <Layout>
+            <Parallax
+                small
+                filter
+                image={require('../../images/profile-bg.jpg')}
+            />
+            <div className={classNames(classes.main, classes.mainRaised)}>
+                <div>
+                    <div className={classes.container}>
+                        <GridContainer justify="center">
+                            <GridItem xs={12} sm={12} md={6}>
+                                <div className={classes.profile}>
+                                    <div>
+                                        <img
+                                            src="https://1.bp.blogspot.com/_bCYQxIvMQ2U/TQOQAXHapkI/AAAAAAAAAQw/dIjtEHBe5cc/s640/anonymous_international_black_sticker-p217501226871431885qjcl_400.jpg"
+                                            alt="..."
+                                            className={imageClasses}
+                                        />
+                                    </div>
+                                    <div className={classes.name}>
+                                        <h3 className={classes.title}>
+                                            {user && user.name}
+                                        </h3>
+                                        <h6>{user && user.type}</h6>
+                                    </div>
+                                </div>
+                            </GridItem>
+                        </GridContainer>
+                        <div className={classes.description}>
+                            <p>{user && user.email}</p>
+                            <p>{user && user.phone}</p>
+                            <br />
+                            <br />
+                        </div>
+                    </div>
                 </div>
-              </GridItem>
-            </GridContainer>
-            <div className={classes.description}>
-              <p>
-                An artist of considerable range, Chet Faker — the name taken by
-                Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs
-                and records all of his own music, giving it a warm, intimate
-                feel with a solid groove structure.{" "}
-              </p>
             </div>
-            <GridContainer justify="center">
-              <GridItem xs={12} sm={12} md={8} className={classes.navWrapper}>
-                <NavPills
-                  alignCenter
-                  color="primary"
-                  tabs={[
-                    {
-                      tabButton: "Studio",
-                      tabIcon: Camera,
-                      tabContent: (
-                        <GridContainer justify="center">
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src={studio1}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={studio2}
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src={studio5}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={studio4}
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                        </GridContainer>
-                      )
-                    },
-                    {
-                      tabButton: "Work",
-                      tabIcon: Palette,
-                      tabContent: (
-                        <GridContainer justify="center">
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src={work1}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={work2}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={work3}
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src={work4}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={work5}
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                        </GridContainer>
-                      )
-                    },
-                    {
-                      tabButton: "Favorite",
-                      tabIcon: Favorite,
-                      tabContent: (
-                        <GridContainer justify="center">
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src={work4}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={studio3}
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src={work2}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={work1}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={studio1}
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                        </GridContainer>
-                      )
-                    }
-                  ]}
-                />
-              </GridItem>
-            </GridContainer>
-          </div>
-        </div>
-      </div>
-    </Layout>
-  );
+        </Layout>
+    )
 }
